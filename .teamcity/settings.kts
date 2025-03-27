8c8b0a5eb58ed382fd1ab0d6c83cc103bf37a2ea
akupreychik <anton.kupreychik@jetbrains.com>
2025-03-27 10:39:12 +0200

    remove comments from settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -9,21 +9,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetTest
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetRestore
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in IntelliJ IDEA, open the 'TeamCity'
-tool window (View -> Tool Windows -> TeamCity), then click the
-'Debug' button in the toolbar and select the desired settings file.
-*/
-
 version = "2019.2"
 
 project {
